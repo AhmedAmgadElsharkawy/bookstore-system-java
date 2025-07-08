e6a34ed08d6c1a07ab7bb1654a278ef6b176d547
AhmedAmgadElsharkawy <ahmedamgad16746@gmail.com>
2025-07-08 20:07:13 +0300

    add demo book

--- src/models/products/DemoBook.java
+++ src/models/products/DemoBook.java
@@ -1,5 +1,14 @@
 package models.products;
 
-public class DemoBook {
-    
+
+public class DemoBook extends Book{
+    public DemoBook(String isbn, String title, int year, double price) {
+        super(isbn, title, year, price);
+    }
+
+    @Override
+    public boolean isPurchasable() {
+        return false;
+    }
+
 }
