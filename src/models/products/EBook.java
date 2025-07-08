146bdfcb577de7418c4594a35b40e1519be94887
AhmedAmgadElsharkawy <ahmedamgad16746@gmail.com>
2025-07-08 20:10:14 +0300

    add the Ebook

--- src/models/products/EBook.java
+++ src/models/products/EBook.java
@@ -1,5 +1,24 @@
 package models.products;
 
-public class EBook {
-    
+public class EBook extends Book {
+    private String filetype;
+
+    public EBook(String isbn, String title, int year, double price, String filetype) {
+        super(isbn, title, year, price);
+        this.filetype = filetype;
+    }
+
+    public void setFiletype(String filetype) {
+        this.filetype = filetype;
+    }
+
+    public String getFiletype(String filetype) {
+        return this.filetype;
+    }
+
+    @Override
+    public boolean isPurchasable() {
+        return true;
+    }
+
 }
