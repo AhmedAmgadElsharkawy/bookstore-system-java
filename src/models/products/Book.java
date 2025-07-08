081c336090b89fc0bd56bb08e650cb3109a321d5
AhmedAmgadElsharkawy <ahmedamgad16746@gmail.com>
2025-07-08 20:03:09 +0300

    add book interface

--- src/models/products/Book.java
+++ src/models/products/Book.java
@@ -1,5 +1,49 @@
 package models.products;
 
 public abstract class Book {
-    
+    private String isbn;
+    private String title;
+    private int year;
+    private double price;
+
+    public Book(String isbn, String title, int year, double price) {
+        this.isbn = isbn;
+        this.title = title;
+        this.year = year;
+        this.price = price;
+    }
+
+    public String getIsbn() {
+        return this.isbn;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public int getYear() {
+        return this.year;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public abstract boolean isPurchasable();
 }
